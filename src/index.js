3f758c6b9bc4faaa1248ec13ca7374e312e11528
Aleksander Voronov <irreligious86@gmail.com>
2021-03-03 19:49:01 +0100

    feat: ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newArr = Math.abs(n).toString().split("");
+    let revNewArr = newArr.reverse();
+    let newString = revNewArr.join("");
+
+    return Number(newString);
+};
